67e0f6ef0956fd7f84b1f9ed2d1e92ce33833fc4
Gabriel Ribeiro <105074547+gabrieszin@users.noreply.github.com>
2023-08-13 11:58:49 -0300

    Add. funcionalidades e scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
 "use strict";
 
+import { SwalAlert } from './modulos/utilitarios.js'
+
 (() => {
-  // hljs.highlightAll();
   
+  try{
+    pdf2htmlEX.defaultViewer = new pdf2htmlEX.Viewer({});
+  }catch(e){}
+
   document.querySelectorAll('[data-recarrega-pagina]').forEach(botao => {
     botao.addEventListener('click', () => {
       window.location.reload();
@@ -22,21 +27,29 @@
         link.href = 'https://github.com/gabrieszin/[nome-repositorio]';
         break;
       }
-
+      
       link.setAttribute('rel', 'noopener noreferrer');
     })
   }
   
   function atribuirAcoes(){
     const acoes = document.querySelectorAll('[data-action]');
-
+    
     acoes.forEach(acao => {
       switch(acao.dataset.action){
         case 'acao':
         break;
-
+        
+        case 'editar':
+        $(acao).on('click', (event) => {
+          event.preventDefault();
+          // SwalAlert('alert', {icon: 'success', title:'Teste', comp:{text:'Isso é apenas um teste', timer:null}, confirmacao:null});
+          document.querySelector('#modal-editar-informacoes').showModal();
+        })
+        break;
+        
         default:
-          console.warn('A ação não foi implementada.')
+        console.warn('A ação não foi implementada.')
         break;
       }
     })
@@ -45,6 +58,39 @@
   window.addEventListener("load", function () {
     $('.overlay').hide();
     atribuirLinks();
-    atribuirLinks();
+    atribuirAcoes();
   });
+  
+  // document.querySelector('#modal-editar-informacoes').showModal();
+  
+  var antes_de_imprimir = function() {
+    // console.log('Antes de imprimir...');
+    $('#controle').hide();
+  };
+  
+  var depois_de_imprimir = function() {
+    // console.log('Depois de imprimir...');
+    $('#controle').show();
+  };
+  
+  if (window.matchMedia) {
+    var mediaQueryList = window.matchMedia('print');
+    mediaQueryList.addListener(function(mql) {
+      if (mql.matches) {
+        antes_de_imprimir();
+      } else {
+        depois_de_imprimir();
+      }
+    });
+  }
+  
+  window.onbeforeprint = antes_de_imprimir();
+  window.onafterprint = depois_de_imprimir();
+  
+  $('.btn-impressao').on('click', (event) => {
+    event.preventDefault();
+    window.print();
+  })
+  
+  console.log($('[refer]'))
 })();
